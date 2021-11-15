a30a0365755bb33dffd2cf1f36f6d06c55b03add
MarcelinB <73071739+MarcelinB@users.noreply.github.com>
2021-11-15 14:06:00 +0100

    Récupération de toutes les tâches

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Task;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TaskController extends AbstractController
 {
@@ -13,6 +14,17 @@ class TaskController extends AbstractController
      */
     public function index(): Response
     {
+        // On va chercher avec Doctrine le Repository de nos task
+
+        $repository = $this->getDoctrine()->getRepository(Task::class);
+
+        // dans ce repository nous recuperons toutes les données
+        $tasks = $repository->findAll();
+
+        // Affichages des donnes dans le var_dumper 
+        //dd($tasks);
+
+
         return $this->render('task/index.html.twig', [
             'controller_name' => 'TaskController',
         ]);
