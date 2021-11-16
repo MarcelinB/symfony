773e3f69ab00626d0c2ec95f2e4c0945192e29b4
MarcelinB <73071739+MarcelinB@users.noreply.github.com>
2021-11-16 16:11:33 +0100

    refactorisation (à revoir)

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -4,41 +4,76 @@ namespace App\Controller;
 
 use App\Entity\Task;
 use App\Form\TaskType;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TaskController extends AbstractController
 {
+    /** 
+     * @var TaskRepository
+     */
+    private $repository;
+
     /**
-     * @Route("/task/listing", name="task")
+     * @var EntityManagerInterface
+     */
+    private $manager;
+
+    public function __construct(TaskRepository $repository, EntityManagerInterface $manager)
+    {
+
+        $this->repository = $repository;
+        $this->manager = $manager;
+    }
+    /**
+     *@Route("/task/listing", name="task_listing")
      */
     public function index(): Response
     {
-        // On va chercher avec Doctrine le Repository de nos task
+        // Dans le repo, on récupère les entrées
+        $tasks = $this->repository->findAll();
 
-        $repository = $this->getDoctrine()->getRepository(Task::class);
-
-        // dans ce repository nous recuperons toutes les données
-        $tasks = $repository->findAll();
-
-        // Affichages des donnes dans le var_dumper 
-        //dd($tasks);
-
+        // Affichage dans le var_dumper
+        // dd($tasks);
 
         return $this->render('task/index.html.twig', [
             'tasks' => $tasks,
         ]);
     }
 
-    /**
-     * @Route("/task/create", name="task_create")
+    /** 
+     *@Route("/task/create", name="task_create")
      */
-    public function creatTask(Request $request)
+    public function task(Task $task = null, Request $request)
     {
-        $task = new Task;
+        if (!$task) {
+            $task = new Task;
+
+            $task->setCreatedAt(new \DateTime());
+        }
+
+
         $form = $this->createForm(TaskType::class, $task, []);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Pas nécessaire
+            // $task->setName($form['name']->getData())
+            //     ->setDescription($form['description']->getData())
+            //     ->setDueAt($form['dueAt']->getData())
+            //     ->setTag($form['tag']->getData());
+
+            // $manager = $this->getDoctrine()->getManager();
+            $this->manager->persist($task);
+            $this->manager->flush();
+
+            return $this->redirectToRoute('task_listing');
+        }
 
         return $this->render('task/create.html.twig', [
             'form' => $form->createView()
