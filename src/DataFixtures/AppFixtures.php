b65b144605d17be8c1b6d094076370ff6423c12f
MarcelinB <73071739+MarcelinB@users.noreply.github.com>
2021-11-16 09:59:13 +0100

    Ajout des catégories

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,9 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Task;
 use Faker\Factory;
+use App\Entity\Tag;
+use App\Entity\Task;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -14,8 +15,25 @@ class AppFixtures extends Fixture
         // Création d'un objet Faker
         $faker = Factory::create('fr_FR');
 
-        //Création entre 15 et 20 tâches aléatoirement
+        //Création de nos 5 catégories
+        for ($c = 0; $c < 5; $c++) {
+            //création d'un nouvel objet tag 
+            $tag = new Tag;
 
+            //On ajoute un nom à notre catégorie
+            $tag->setName($faker->colorName());
+
+            //On fait persister les données
+            $manager->persist($tag);
+        }
+
+        // On push les catégories en BDD
+        $manager->flush();
+
+        //récupération des catégories créées
+        $tags = $manager->getRepository(Tag::class)->findAll();
+
+        //Création entre 15 et 20 tâches aléatoirement
         for ($t = 0; $t < mt_rand(15, 30); $t++) {
             // Création d'un nouvel objet Task
             $task = new Task;
@@ -24,11 +42,13 @@ class AppFixtures extends Fixture
             $task->setName($faker->sentence(6))
                 ->setDescription($faker->paragraph(3))
                 ->setCreatedAt(new \DateTime())
-                ->setDueAt($faker->dateTimeBetween('now', '6 months'));
+                ->setDueAt($faker->dateTimeBetween('now', '6 months'))
+                ->setTag($faker->randomElement($tags));
 
             // On fait persister les données
             $manager->persist($task);
         }
+
 
         $manager->flush();
     }
